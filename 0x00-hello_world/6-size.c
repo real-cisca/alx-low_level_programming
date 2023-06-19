ca2be313f8aa5dd7d950403349c5efe4021a7376
francisca_obinali <franciscaobinali16@gmail.com>
2023-06-19 16:38:56 -0700

    seventh task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu bytes\n", sizeof(char));
+	printf("Size of short: %lu bytes\n", sizeof(short));
+	printf("Size of int: %lu bytes\n", sizeof(int));
+	printf("Size of long: %lu bytes\n", sizeof(long));
+	printf("Size of long long: %lu bytes\n", sizeof(long long));
+	printf("Size of float: %lu bytes\n", sizeof(float));
+	printf("Size of double: %lu bytes\n", sizeof(double));
+	printf("Size of long double: %lu bytes\n", sizeof(long double));
+
+	return (0);
+}
