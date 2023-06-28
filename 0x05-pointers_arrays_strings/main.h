9b00470c4251fae4d389b077b8229ff320187168
francisca_obinali <franciscaobinali16@gmail.com>
2023-06-28 06:04:04 -0700

    strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,10 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include<unistd.h>
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -14,5 +13,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+void swap_int(int *a, int *b);
 
 #endif
