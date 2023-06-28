2dceba9182c6cbf1f87f6a354d845e72df0f4507
francisca_obinali <franciscaobinali16@gmail.com>
2023-06-28 05:16:29 -0700

    Update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints with a new line to the standard error
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 20155-10-19\n", stdout);
+	return (1);
+}
