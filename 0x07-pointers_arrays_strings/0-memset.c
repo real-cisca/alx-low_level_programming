8daec4f40e23840eb55b4d032213d75c1348e4ee
francisca_obinali <franciscaobinali16@gmail.com>
2023-07-04 14:53:13 -0700

    1st task redo

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_memset(char *s, char b, unsigned int n) {
-  char *p = s;
-  while (n--) {
-    *p++ = b;
-  }
-  return s;
-}
 
+char *_memset(char *s, char b, unsigned int n) {
+    char *ptr = s;
+
+    while (n > 0) {
+        *ptr = b;
+        ptr++;
+        n--;
+    }
+
+    return s;
+}
