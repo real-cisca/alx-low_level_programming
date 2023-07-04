295fd65de154e15afff45753a4c32c82f1563060
francisca_obinali <franciscaobinali16@gmail.com>
2023-07-04 13:55:05 -0700

    7th task

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,27 +1,13 @@
+#include "main.h"
 #include <stdio.h>
 
-void print_chessboard() {
-    int size = 8;   
-    char board[size][size];
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            board[i][j] = ' ';
-        }
+void print_chessboard(char (*a)[8]) {
+  int i;
+  int j;
+	for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      printf("%c ", a[i][j]);
     }
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if ((i + j) % 2 == 0) {
-                board[i][j] = '#';
-            }
-        }
-    }
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            printf("%c ", board[i][j]);
-        }
-        printf("\n");
-    }
+    printf("\n");
+  }
 }
