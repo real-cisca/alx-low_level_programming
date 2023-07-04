d73b01092e79c9792911daa601336d696f645976
francisca_obinali <franciscaobinali16@gmail.com>
2023-07-04 00:19:09 -0700

    fourth task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,15 @@
-using System;
+#include <stdio.h>
 
-public static class MemoryHelper
-{
-public static unsafe byte* _memset(byte* s, byte b, uint n)
-{
-    for (uint i = 0; i < n; i++)
-    {
-        *(s + i) = b;
+char *_memset(char *s, char b, unsigned int n) {
+       unsigned char* p = s;
+    unsigned char value = (unsigned char)b;
+
+    while (n > 0) {
+        *p = value;
+        p++;
+        n--;
     }
 
     return s;
-} 
 }
 
-static unsafe void Main()
-{
-    byte[] array = new byte[10];
-
-    fixed (byte* ptr = array)
-    {
-        byte* result = MemoryHelper._memset(ptr, 65, 10);
-        Console.WriteLine(BitConverter.ToString(array));
-    }
-}
