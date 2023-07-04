b0864231fb24c5aff4cf6e1242761891d5b60970
francisca_obinali <franciscaobinali16@gmail.com>
2023-07-04 13:22:02 -0700

    4th task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strpbrk(char *s, char *accept) {
+    while (*s != '\0') {
+        char *a = accept;
+        while (*a != '\0') {
+            if (*s == *a) {
+                return s; 
+            }
+            a++;
+        }
+        s++;
+    }
+    return NULL;
+}
